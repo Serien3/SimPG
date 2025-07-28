50a5d7d991205c931dfefd215b0f6e952a7073c3
HWangHIT <whhit1825@outlook.com>
2025-07-28 16:05:20 +0800

    Fix subpackage import error

--- src/SimPG/utils/__init__.py
+++ src/SimPG/utils/__init__.py
@@ -2,8 +2,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-from set_default_logging import set_default_logging
-from sim_part import *
+from .set_default_logging import set_default_logging
+from .sim_part import *
 
 
 __all__ = ["set_default_logging", "sim_part", "sim_part_for_num"]
