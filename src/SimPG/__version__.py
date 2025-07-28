00a2e583b319ef4ea386ce0220749d43ac8dfb5b
Serien3 <whhit1825@outlook.com>
2025-07-28 15:16:22 +0800

    Update __version__.py(description)

--- src/SimPG/__version__.py
+++ src/SimPG/__version__.py
@@ -3,7 +3,7 @@ __version__ = "1.0.0"
 __author__ = "Serien3 "
 __author_email__ = "whhit1825@outlook.com"
 __description__ = (
-    "A population genome simulation tool based on large-scale pangenomic data"
+    "A population-specific haplotype genome simulation tool developed based on pangenome data"
 )
 __url__ = "https://github.com/Serien3/SimPG"
 __license__ = "MIT"
